17ab324fb81e8e1924d92e2172c4821e15d69151
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-05-16 15:34:21 -0300

    teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -20,6 +20,7 @@ public class Program {
 		String holder = sc.nextLine();
 		System.out.println("Is there an initial deposit (y/n)? ");
 		char response = sc.next().charAt(0);
+		
 		if (response == 'y') {
 			System.out.println("Enter initial deposit value: ");
 			double initialDeposit = sc.nextDouble();
